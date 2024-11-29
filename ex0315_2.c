73ff96fc4a37513ca7c2fa830d04bf56e4725f3f
congcoding <congcoding@mail.com>
2024-11-29 09:31:44 +0900

    message

---
+++ ex0315_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*3월 15일 실습예제
+	c언어 기초 */
+
+	printf("사칙연산 \n");
+	printf("10+3=%d\n", 10 + 3);
+	printf("%d\n", 10 - 3);
+	printf("%d\n", 10 * 3);
+	printf("%f\n", 10.0 / 3);
+	printf("%.2f\n", 10.0 / 3);
+	printf("%d\n", 10 % 3);
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("9x1=%d\n", 9 * 1);
+	printf("9x2=%d\n", 9 * 2);
+	printf("9x3=%d\n", 9 * 3);
+	printf("9x4=%d\n", 9 * 4);
+	printf("9x5=%d\n", 9 * 5);
+	printf("9x6=%d\n", 9 * 6);
+	printf("9x7=%d\n", 9 * 7);
+	printf("9x8=%d\n", 9 * 8);
+	printf("9x9=%d\n", 9 * 9);
+
+
+	return 0;
+}
